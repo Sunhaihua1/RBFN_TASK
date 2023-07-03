6af4260300281002c32221ac2b851df42c6d603a
Sunhaihua1 <2660245132@qq.com>
2023-07-04 01:17:14 +0800

    fix some stupid bugs

--- RBFN.py
+++ RBFN.py
@@ -5,11 +5,11 @@ class RBFN:
 	def __init__(self,input_size, kernal_size) -> None:
 		self.input_size = input_size
 		self.kernal_size = kernal_size
-		self.sigma = None
+		self.sigma = 0.5
 		self.w = None
 		self.b = None
 		self.centers = None
-		self._kernal_function = lambda center, x_point: np.exp(-self.sigma * np.linalg.norm(center-x_point)**2) # gaussian kernal
+		self._kernal_function = lambda center, x_point: np.exp(-0.5 * np.power(np.linalg.norm(center-x_point) / self.sigma, 2)) # gaussian kernal
 
 	def _calculate_interpolation_matrix(self, X):
 		G = np.zeros((X.shape[0], self.kernal_size))
@@ -18,17 +18,26 @@ class RBFN:
 				G[i, j] = self._kernal_function(self.centers[j], X[i])
 		return G
 
+	def calculate_sigma(self):
+		max = 0
+		num = 0
+		total = 0
+		for i in range(self.kernal_size - 1):
+			for j in range(i + 1, self.kernal_size):
+				dis = np.linalg.norm(self.centers[i] - self.centers[j])
+				total += dis
+				num += 1
+		self.sigma = 2 * total / num
 	def fit(self, X, Y) :
      	# step 1: initialize centers
 		self.centers = KMeans(n_clusters=self.kernal_size).fit(X).cluster_centers_
 
 		# step 2: calculate sigma
-		dmax = max([np.linalg.norm(self.centers[i]-self.centers[j]) for i in range(self.kernal_size) for j in range(self.kernal_size)])
-		self.sigma = dmax / np.sqrt(2*self.kernal_size)
-
+		# dmax = max([np.linalg.norm(self.centers[i]-self.centers[j]) for i in range(self.kernal_size) for j in range(self.kernal_size)])
+		# self.sigma = dmax / np.sqrt(2*self.kernal_size)
+		self.calculate_sigma()
 		# step 3: calculate interpolation matrix
 		G = self._calculate_interpolation_matrix(X)
-
 		# step 4: calculate weights
 		self.w = np.dot(np.linalg.pinv(G), Y)
 	
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+	# print(x_train.shape, y_train.shape, x_test.shape)
