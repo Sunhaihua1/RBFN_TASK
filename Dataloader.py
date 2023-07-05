5c802143a3697f17b0530fcc1e720e18b5918c9d
Sunhaihua1 <2660245132@qq.com>
2023-07-05 10:45:53 +0800

    data_loader format code

--- Dataloader.py
+++ Dataloader.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import torch
 from RBFN import RBFN
 import warnings
 warnings.filterwarnings("ignore")
@@ -10,8 +9,5 @@ def load_data(src):
 	x_train = data[:, 0:10]
 	y_train = data[:, 11]
 	x_test = data[:, 14:24]
-	x_train = torch.tensor(x_train, dtype=torch.float32)
-	y_train = torch.tensor(y_train, dtype=torch.float32)
-	x_test = torch.tensor(x_test, dtype=torch.float32)
 	return x_train, y_train, x_test
 
